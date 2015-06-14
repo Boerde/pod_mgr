686ac4de203b52363f8608002dedd8096f4edd27
boerde <boerde@localhost.localdomain>
2015-06-14 06:19:27 +0200

    tmp2

--- db/migrate/20150614013351_add_paperclip_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPaperclipToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :file, :paperclip
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613191721) do
+ActiveRecord::Schema.define(version: 20150614023731) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
   end
 
 end
